35a8f1e109b4102feabbc7e6ff2ab1ac1eea7110
Faiyt <unfaiyted@gmail.com>
2025-03-06 17:20:32 -0600

    chore: adjust api entry point

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -7,5 +7,5 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 console.log('API_BASE_URL', API_BASE_URL);
 // Create a typed client
 export const { GET, POST, PUT, DELETE } = createClient<paths>({
-	baseUrl: API_BASE_URL + '/v1'
+	baseUrl: '/api/v1'
 });
